4575c5ca25c58b8f5ace58d36d55c565eefce7ed
Ishita Rakshit <ishitarakshit@PIs-MacBook-Pro.local>
2017-04-18 14:39:33 -0400

    controller updates

--- src/main/java/com/samples/demo/config/AppConfig.java
+++ src/main/java/com/samples/demo/config/AppConfig.java
@@ -1,18 +1,12 @@
 package com.samples.demo.config;
 
-import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
-import com.samples.demo.controller.HelloWorldRestController;
-
 @Configuration
 @EnableWebMvc
-
+@ComponentScan("com.samples.demo.controller")
 public class AppConfig {
-	@Bean
-	public HelloWorldRestController helloWorldRestController(){
-		return new HelloWorldRestController();
-	}
 
 }
--- src/main/java/com/samples/demo/controller/HelloWorldRestController.java
+++ src/main/java/com/samples/demo/controller/HelloWorldRestController.java
@@ -1,14 +1,18 @@
 package com.samples.demo.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.samples.demo.model.Greeting;
 
 @RestController
 public class HelloWorldRestController {
 	
 	@RequestMapping("/greeting")
-	public String greeting() {
-		return "Hello World!";
+	public Greeting greeting(@RequestParam("type") String type, @RequestParam("name") String name) {
+		Greeting greeting = new Greeting(type, name);
+		return greeting;
 	}
 
 }
